70f3b28fa568b4f699c65dcd99b5e84e3e558fe3
Droid-M <62139366+Droid-M@users.noreply.github.com>
2020-10-17 20:04:37 -0300

    Add all sections
    Sections: "about", "services", "team", "portfolio" and "contact".

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-//Geral
+$(document).ready(function() {
+
+  $('.filter-btn').on('click', function() {
+    let type = $(this).attr('id');
+    let box = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+    findBox(type, box);
+  });
+
+  function findBox(type, box) {
+    if (type == 'all') {
+      box.fadeIn();
+    }
+    else {
+      box.each (function () {
+        if (!$(this).hasClass(type)) {
+          $(this).fadeOut();
+        }
+        else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+
+  $('.main-btn').on('click', function() {
+    let scroll = "#" + $(this).attr('name');
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(scroll).offset().top - 140
+    }, 1200);
+  });
+
+  $('.nav-item').click(function() {
+    let scroll = "#" + $(this).attr('name');
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(scroll).offset().top - 140
+    }, 1200);
+  });
+
+});
